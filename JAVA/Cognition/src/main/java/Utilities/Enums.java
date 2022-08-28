a2cf02de5a1120a22ae1496902e241f83024bb94
Thom Simmons <thomedy@thinkrium.com>
2022-08-28 12:34:52 -0500

    Creating the first layer and prediction generation

---
+++ JAVA/Cognition/src/main/java/Utilities/Enums.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+/**
+ *   A list of static enums to white box any choices made internally
+ *
+ **/
+
+public class Enums {
+    /**
+     * Display level for Logging
+     */
+    public static enum Display_Level {All, Nerual_Nodes, Weights, Biases};
+}

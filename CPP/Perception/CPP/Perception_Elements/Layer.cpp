72c42f22689afc58522542554f48a2764f6c5019
Thom Simmons <thomedy@thinkrium.com>
2022-11-18 07:29:02 -0600

    adding a random number generator

--- CPP/Perception/CPP/Perception_Elements/Layer.cpp
+++ CPP/Perception/CPP/Perception_Elements/Layer.cpp
@@ -13,8 +13,41 @@ Perception::Layer::Layer::Layer()
 /// Creates an Layer of Neural Nodes by the count of param_node_count
 /// </summary>
 Perception::Layer::Layer::Layer(int param_node_count) {
+    this->Neural_Node_Count = param_node_count;
+}
+
+/// <summary>
+/// Sets the node count
+/// </summary>
+/// <param name="param_node_count"></param>
+void Perception::Layer::Layer::Set_Node_Count(int param_node_count) { 
+    this->Neural_Node_Count = param_node_count;
+}
+
+int Perception::Layer::Layer::Get_Node_Count() {
+    return this->Neural_Node_Count;
+}
+
+void Perception::Layer::Layer::Create_Layer_Nodes() {
+    this->Create_Nodes_By_Count();
+}
+
+void Perception::Layer::Layer::Create_Nodes_By_Count() {
+    Neural_Nodes.push_back(
+        this->Generate_Random_Numerical_Value()
+    );
+}
+
+
+void Perception::Layer::Layer::Create_Nodes_By_Count(int param_node_count) {
 
 }
+
+
+short Perception::Layer::Layer::Generate_Random_Numerical_Value() {
+    return Test_Return_Data;
+}
+
 
 /// <summary>
 /// Destructor
--- CPP/Perception/Headers/Perception_Elements/Layer.h
+++ CPP/Perception/Headers/Perception_Elements/Layer.h
@@ -4,6 +4,7 @@
 #include <Root.h>
 #include <Neural_Node.h>
 #include <vector>
+#include <random>
 
 using namespace Utilities;
 using namespace std;
@@ -14,11 +15,19 @@ namespace Perception {
 		{
 			public:
 				Layer();
+				Layer(int param_node_count);
+				void Set_Node_Count(int param_node_count);
+				int Get_Node_Count();
+				void Create_Layer_Nodes();
+				void Create_Nodes_By_Count();
+				void Create_Nodes_By_Count(int param_node_count);
+				short Generate_Random_Numerical_Value();
 				~Layer();
 
 			private:
 		
-				vector<Neural_Node> Node;
+				int Neural_Node_Count;
+				vector<Neural_Node> Neural_Nodes;
 			};    
     }
 }

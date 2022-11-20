c8576a1c5615f06e8766c728bae222884e9e4e0a
Thom Simmons <thomedy@thinkrium.com>
2022-11-20 00:02:17 -0600

    added testing

--- CPP/Perception/Testing/Testing_Layer.cpp
+++ CPP/Perception/Testing/Testing_Layer.cpp
@@ -1,5 +1,18 @@
 #include <gtest/gtest.h>
 
-TEST(Perception_Test, Testing_Random_Number_Generator) {
+TEST(Perception_Test, Testomg_True) {
+	EXPECT_TRUE(true);
+}
+ 
+TEST(Perception_Test, some_other_thing) {
+	EXPECT_TRUE(true);
+}
+
+
+TEST(Perception_Test, asdfa) {
+	EXPECT_TRUE(true);
+}
+
+TEST(Perception_Test, ssdfsdfasdfasd) {
 	EXPECT_TRUE(true);
 }
